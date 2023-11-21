c9ffd59a5eb16ca0a303b513a252f0ea29dfcf63
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-21 14:00:07 +0100

    added form

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// if (eta < 18){
+//     full_price = full_price * 0.8;
+//     console.log(full_price);
+// }
+
+// else if (eta > 65){
+//     full_price = full_price * 0.6;
+//     console.log(full_price);
+// }
+
+// else{
+//     full_price = full_price
+// }
